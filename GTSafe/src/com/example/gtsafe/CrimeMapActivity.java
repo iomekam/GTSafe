17b20c878a4bb9f882968343289ec6be065cb3c2
Rafael Santillan <rsantllan6@gatech.edu>
2014-03-10 10:28:52 -0400

    Added new markers for the map and started creating new zones. Additionally, as soon as the database is fixed, each marker should be matching with the respective crime with an informative pop up window

--- GTSafe/src/com/example/gtsafe/CrimeMapActivity.java
+++ GTSafe/src/com/example/gtsafe/CrimeMapActivity.java
@@ -4,9 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SlidingDrawer;
 import android.widget.Toast;
 
 import com.example.gtsafe.library.MapHelper;
@@ -22,6 +27,8 @@ import com.google.android.gms.maps.model.PolygonOptions;
 public class CrimeMapActivity extends Activity {
 private GoogleMap mMap;
 private MapHelper helper;
+private SlidingDrawer slider;
+private Button filterButt;
 private LatLngBounds coords = new LatLngBounds(
     new LatLng(33.770836, -84.407272), new LatLng(33.786638, -84.390492));
 
@@ -34,10 +41,12 @@ setContentView(R.layout.activity_map);
 try {
     // Loading map
     initilizeMap();
+    
 
 } catch (Exception e) {
     e.printStackTrace();
 }
+
 }
 
 private void initilizeMap() {
@@ -47,6 +56,7 @@ if (mMap == null) {
     mMap.setMyLocationEnabled(true);
     helper = new MapHelper(mMap);
     mMap = helper.populateZones();
+    mMap = helper.populateCrimes();
     mMap.setOnMapClickListener(new OnMapClickListener() {
 
         @Override
@@ -55,26 +65,32 @@ if (mMap == null) {
             //marker.remove();
             //drawMarker(point);
         	
-        }
-    });
+        }   
+    }); 
 
-    if (mMap == null) {
+    if ( mMap == null) {
         Toast.makeText(getApplicationContext(),
-                "Sorry! unable to create maps", Toast.LENGTH_SHORT)
-                .show();
+                 "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                 .show();
     }
     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords.getCenter(), 15));
 }
     else{
     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords.getCenter(), 15));
 }
+	filterButt = (Button)findViewById(R.id.ok);
+	filterButt.setOnClickListener(new View.OnClickListener() {
+	 @Override
+	 public void onClick(View v) {
+
+	 }
+	 });
 }
 @Override
 protected void onResume() {
 super.onResume();
 initilizeMap();
 }
-
 
 
 }
--- GTSafe/src/com/example/gtsafe/library/MapHelper.java
+++ GTSafe/src/com/example/gtsafe/library/MapHelper.java
@@ -1,13 +1,23 @@
 package com.example.gtsafe.library;
 
+import java.sql.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import android.graphics.Color;
 
+import com.example.gtsafe.R;
+import com.example.gtsafe.library.listeners.interfaces.OnDBGetListener;
+import com.example.gtsafe.model.CrimeData;
 import com.example.gtsafe.model.ZoneData;
 import com.example.gtsafe.model.ZoneInfo;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 
@@ -15,6 +25,7 @@ public class MapHelper {
 	final DBManager db = DBManager.getInstance();
 	private List<ZoneData> zones;
 	private GoogleMap map;
+	private List<CrimeData> crimes;
 	
 	public MapHelper(GoogleMap mappy)
 	{
@@ -30,9 +41,52 @@ public class MapHelper {
 		            .addAll(zones.get(x).getLocation())
 		            .strokeColor(Color.BLACK)
 		            .fillColor(Color.argb(50, 200, 0, 0)));
+				    
+			    	double xcentroid = 0; 
+			    	double ycentroid = 0;
+				    List<LatLng> coords = zones.get(x).getLocation();
+				    for(int i = 0; i < coords.size(); i++){
+				    	xcentroid+= coords.get(i).latitude;
+				    	ycentroid+= coords.get(i).longitude;
+				    }
+				    xcentroid = xcentroid/coords.size();
+				    ycentroid = ycentroid/coords.size();
+				    Marker zoneIterator = this.map.addMarker(new MarkerOptions()
+                    .position(new LatLng(xcentroid, ycentroid))
+                    .title("Zone: " + x)
+                    .snippet("" + zones.get(x).getZoneInformation().getDescription()));
 				}
 			}
 		}
+		
 		return this.map;
+	}
+	
+	public GoogleMap populateCrimes(){
+		db.getAllCrimeData(new OnDBGetListener<CrimeData>(){
+
+			@Override
+			public void OnGet(List<CrimeData> list) {
+				crimes = list;
+				for(int x = 0; x < crimes.size() /20; x++){
+					  Marker tempMarker = map.addMarker(new MarkerOptions()
+                      .position(crimes.get(x).getLocation())
+                      .title("Rape")
+                      .snippet(crimes.get(x).getLocationName())
+                      .icon(BitmapDescriptorFactory.fromResource(R.drawable.rape)));
+//					 Circle circle = map.addCircle(new CircleOptions()
+//				     .center(crimes.get(x).getLocation())
+//				     .radius(10)
+//				     .strokeColor(Color.RED)
+//				     .fillColor(Color.BLUE));
+				}
+			}
+		});
+		 Circle circle = this.map.addCircle(new CircleOptions()
+	     .center(new LatLng(33.775618,-84.396285))
+	     .radius(10)
+	     .strokeColor(Color.RED)
+	     .fillColor(Color.BLUE));	
+		 return this.map; 
 	}
 }
