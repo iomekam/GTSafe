45432f7fbef2fb22e5b1efd27df1487cd54f5b2f
Ikenna Omekam <iomekam3@gatech.edu>
2014-03-14 01:03:47 -0400

    Sorted clery acts and fixed getallcleryact method

--- GTSafe/src/com/example/gtsafe/library/DBManager.java
+++ GTSafe/src/com/example/gtsafe/library/DBManager.java
@@ -526,13 +526,18 @@ public class DBManager {
 			}
 		}
 		
+		c.close();
+		instance.closeDatabase();
+		
 		return data;
 	}
 	
 	public List<CleryActModel> getAllCleryActs()
 	{
 		List<CleryActModel> dataList = new LinkedList<CleryActModel>();
-		String selectQuery = "SELECT ca_id FROM clery_acts";
+		
+		SQLiteDatabase db = instance.openDatabase();
+		String selectQuery = "SELECT ca_id FROM clery_acts ORDER BY ca_date DESC";
 		
 		Cursor c = db.rawQuery(selectQuery, null);
 		boolean hasNext = c.moveToFirst();
