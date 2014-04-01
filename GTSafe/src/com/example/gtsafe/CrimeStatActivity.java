ba990cb36eefed235571c8250f1dfb3bdd18494e
sokala3 <stephenokala@gmail.com>
2014-04-01 18:03:08 -0400

    Call feature

--- GTSafe/src/com/example/gtsafe/CrimeStatActivity.java
+++ GTSafe/src/com/example/gtsafe/CrimeStatActivity.java
@@ -2,11 +2,27 @@ package com.example.gtsafe;
 
 
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.PointLabelFormatter;
@@ -15,18 +31,119 @@ import com.androidplot.xy.XYPlot;
 import com.androidplot.xy.XYSeries;
 import com.example.gtsafe.library.DBHelper;
 import com.example.gtsafe.library.DBManager;
+import com.example.gtsafe.library.listeners.interfaces.Listable;
 import com.example.gtsafe.library.listeners.interfaces.OnDBGetListener;
+import com.example.gtsafe.library.listeners.interfaces.OnDBUpdateListener;
 import com.example.gtsafe.model.CrimeData;
 import com.example.gtsafe.model.OffenseType;
+import com.example.gtsafe.model.ZoneData;
 
 public class CrimeStatActivity extends SuperActivity {
+	
+	private enum Search
+	{
+		ALL("All Crimes", null), 
+				CRIME_TYPE("Crime Type", OffenseType.values());
+		
+		String name;
+		Listable[] searchContent = {}; //all items should implement Listable
+		
+		private Search(String name, Listable[] searchContent)
+		{
+			this.name = name;
+			
+			if(searchContent != null)
+			{
+				this.searchContent = searchContent;
+			}
+		}
+		
+		public String[] getContent() 
+		{ 
+			List<String> list = new LinkedList<String>();
+			for(Listable item: searchContent)
+			{
+				list.add(((Listable)item).listString());
+			}
+			
+			return list.toArray(new String[list.size()]);
+		}
+		
+		@Override
+		public String toString(){ return name; }
+
+	}
+	
 	private XYPlot plot;
+	private static List<CrimeData> crimeData = new LinkedList<CrimeData>();
+	CustomSpinner searchCrimes;
+	CharSequence result;
+	ArrayAdapter<CrimeData> adapter;
+	Search currentSelection = Search.CRIME_TYPE;
+	Object selectedItem = null;
+	ListView crimes;
+
+
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crime_stat);
-		final DBManager db = DBManager.getInstance();
+		
+		adapter = new ArrayAdapter<CrimeData>(CrimeStatActivity.this,
+				android.R.layout.simple_list_item_1, android.R.id.text1,
+				new LinkedList<CrimeData>());
+		
+		crimes = (ListView) findViewById(R.id.crimeLog);
+		searchCrimes = (CustomSpinner) findViewById(R.id.filterCrime);
+		
+		
+		final ArrayAdapter<Search> spinnerArrayAdapter = new ArrayAdapter<Search>(
+				this, android.R.layout.simple_spinner_item, Search.values());
+		spinnerArrayAdapter
+		.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		
+		searchCrimes.setAdapter(spinnerArrayAdapter);
+		searchCrimes.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int position, long id) 
+			{
+				Search type = (Search)searchCrimes.getItemAtPosition(position);
+				currentSelection = type;
+				
+				if(type == Search.ALL)
+				{
+					adapter.clear();
+					adapter.addAll(crimeData);
+					adapter.notifyDataSetChanged();
+				}
+				else
+				{
+					showPopUp(type.toString(), type.getContent(), type);
+					searchCrimes.setSelection(-1);
+				}
+			}
+			
+		
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {}
+			
+		});
+		
+		
+		
+		/*manager.getCrimesByDate(d1, d2,new OnDBGetListener<CrimeData>(){
+			@Override
+			public void OnGet(List<CrimeData> list) 
+			{
+				crimeData.addAll(list);
+			}
+		});*/
+		/*final DBManager db = DBManager.getInstance();
 		
 		db.getCrimesByType(OffenseType.NON_CRIME,  new OnDBGetListener<CrimeData>(){
 
@@ -36,11 +153,40 @@ public class CrimeStatActivity extends SuperActivity {
 				//Toast.makeText(getApplicationContext(), "Crimes Length: " + list.size(),
 						   //Toast.LENGTH_LONG).show();
 			}
-		});
+		});*/
 		// fun little snippet that prevents users from taking screenshots
         // on ICS+ devices :-)
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
-                                // WindowManager.LayoutParams.FLAG_SECURE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+                                 WindowManager.LayoutParams.FLAG_SECURE);
+        manager.setOnCrimeUpdateEventListener(new OnDBUpdateListener<CrimeData>()
+        		{
+        			@Override
+        			public void OnUpdate(CrimeData item) {
+        				int position = crimes.getSelectedItemPosition();
+        				
+        				if(
+        						(currentSelection == Search.CRIME_TYPE && item.getOffense() == (OffenseType)selectedItem) ||
+        						(currentSelection == Search.ALL))
+        				{
+        				
+        					adapter.insert(item, 0);
+        					adapter.sort(CrimeData.getComparator());
+        					
+        					runOnUiThread(new Runnable() {
+        				        @Override
+        				        public void run() {
+        				                adapter.notifyDataSetChanged();
+        				        }
+        				    });
+        				}
+        				
+        				crimes.setSelection(position);
+        				
+        				crimeData.add(item);
+        				Collections.sort(crimeData, CrimeData.getComparator());
+        			}
+        		});
+        	
  
  
         // initialize our XYPlot reference:
@@ -85,6 +231,53 @@ public class CrimeStatActivity extends SuperActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.crime_stat, menu);
 		return true;
+	}
+	public void showPopUp(String title, final String[] options, Search type) {
+		AlertDialog.Builder b = new Builder(this);
+		b.setTitle(title);
+		
+		
+		if(type == Search.CRIME_TYPE)
+		{
+			b.setItems(options, new OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int position) {
+					dialog.dismiss();
+					adapter.clear();
+					final OffenseType offType = OffenseType.getOffenseType(options[position]);
+					selectedItem = offType;
+					manager.getCrimesByType(offType, new OnDBGetListener<CrimeData>() {
+						@Override
+						public void OnGet(List<CrimeData> list) {
+							adapter.addAll(list);
+							adapter.notifyDataSetChanged();
+						}
+					});
+					Calendar date = Calendar.getInstance();//gives current date at given time
+					date.set(Calendar.YEAR, 2012 );
+				    date.set(Calendar.MONTH, Calendar.JANUARY );
+				    date.set(Calendar.DATE, 01 );
+				    
+				    Calendar dateB = Calendar.getInstance();
+					dateB.set(Calendar.YEAR, 2012 );
+				    dateB.set(Calendar.MONTH, Calendar.DECEMBER );
+				    dateB.set(Calendar.DATE, 31 );
+					manager.getCrimesByDate(new java.sql.Date(date.getTimeInMillis()), new java.sql.Date(dateB.getTimeInMillis()), new OnDBGetListener<CrimeData>() {
+						@Override
+						public void OnGet(List<CrimeData> list1) {
+							for(int i = 0;i< list1.size();i++){
+								Log.w(DISPLAY_SERVICE, "" + (list1.get(i)));
+								
+								
+							}
+						}
+					});
+				}
+			});
+		}
+		
+		b.show();
 	}
 
 }
--- GTSafe/src/com/example/gtsafe/MainActivity.java
+++ GTSafe/src/com/example/gtsafe/MainActivity.java
@@ -7,8 +7,12 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -33,6 +37,8 @@ public class MainActivity extends SuperActivity {
 	private Button view_Button;
 	private Button data_Button;
 	private ImageButton help_Button; // Omar created this.
+	private Button call_Button;
+
     
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -189,5 +195,68 @@ public class MainActivity extends SuperActivity {
 			        startActivity(myIntent);
 			 }
 			 });
+		// add PhoneStateListener
+				PhoneCallListener phoneListener = new PhoneCallListener();
+				TelephonyManager telephonyManager = (TelephonyManager) this
+					.getSystemService(Context.TELEPHONY_SERVICE);
+				telephonyManager.listen(phoneListener,PhoneStateListener.LISTEN_CALL_STATE);
+		call_Button = (Button) findViewById(R.id.callbutton);
+		call_Button.setOnClickListener(new View.OnClickListener() {
+			 
+			@Override
+			public void onClick(View v) {
+ 
+				Intent callIntent = new Intent(Intent.ACTION_CALL);
+				callIntent.setData(Uri.parse("tel:4048942500"));
+				startActivity(callIntent);
+ 
+			}
+ 
+		});
+
 	}
+	
+	//monitor phone call activities
+		private class PhoneCallListener extends PhoneStateListener {
+	 
+			private boolean isPhoneCalling = false;
+	 
+			String LOG_TAG = "LOGGING 123";
+	 
+			public void onCallStateChanged(int state, String incomingNumber) {
+	 
+				if (TelephonyManager.CALL_STATE_RINGING == state) {
+					// phone ringing
+					Log.i(LOG_TAG, "RINGING, number: " + incomingNumber);
+				}
+	 
+				if (TelephonyManager.CALL_STATE_OFFHOOK == state) {
+					// active
+					Log.i(LOG_TAG, "OFFHOOK");
+	 
+					isPhoneCalling = true;
+				}
+	 
+				if (TelephonyManager.CALL_STATE_IDLE == state) {
+					// run when class initial and phone call ended, 
+					// need detect flag from CALL_STATE_OFFHOOK
+					Log.i(LOG_TAG, "IDLE");
+	 
+					if (isPhoneCalling) {
+	 
+						Log.i(LOG_TAG, "restart app");
+	 
+						// restart app
+						Intent i = getBaseContext().getPackageManager()
+							.getLaunchIntentForPackage(
+								getBaseContext().getPackageName());
+						i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+						startActivity(i);
+	 
+						isPhoneCalling = false;
+					}
+	 
+				}
+			}
+		}
 }
